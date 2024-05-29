7e9524d1c8917f6906966c9c1ee51af2e54e4e7a
Monish <164910588+Monish-5@users.noreply.github.com>
2024-05-29 10:17:50 +0530

    Create middelpade.js

---
+++ middelpade.js
@@ -0,0 +1,27 @@
+import MiddelPageCSS from"./Middel.module.css";
+
+function MiddelPage(){
+    return(
+        <>
+        <div className={MiddelPageCSS.p1}>
+
+            <p className={MiddelPageCSS.para}>
+            I am Monish Fullstack Devloper <br></br>
+              I am Study BE in ECE in Hindusthan collage of engineering<br></br>
+
+
+              <button>My resume </button>
+            </p>
+            
+        </div>
+        <div className={MiddelPageCSS.p1}>
+
+            <p className={MiddelPageCSS.para}> About me<br></br>
+             I am monish  studying final year Engeringing in electronic and communities engineering in hindusthan college of Engeringing and technology in Coimbatore. Now I am search for job in full stack development
+            </p>
+        </div>
+
+        </>
+    );
+}
+export default MiddelPage
